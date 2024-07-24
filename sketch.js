aa624d5e50a32e4419140cc137e00fabe8dd64e5
gigantex777 <pablo.angelo@escola.pr.gov.br>
2024-07-24 08:10:26 -0300

    sketch.js

---
+++ sketch.js
@@ -0,0 +1,44 @@
+function setup() {
+  createCanvas(400, 600);
+}
+
+function draw() {
+  background(220);
+  triangle(350, 100, 200, 500);
+  fill("black")
+  circle(00, 00, 100, 150);
+  fill("red")
+  rect(00, 550, 100, 150);
+  fill("red")
+  rect(00, 400, 100, 150);
+  fill("red")
+  rect(00, 250, 100, 150);
+  fill("red")
+  rect(00, 100, 100, 150);
+  fill("blue");
+  rect(100, 100, 200, 500);
+  fill("black");
+  square(120, 130, 30);
+  square(120, 190, 30);
+  square(120, 250, 30);
+  square(120, 310, 30);
+  square(120, 370, 30);
+  square(120, 430, 30);
+  square(120, 490, 30);
+  square(180, 130, 30);
+  square(180, 190, 30);
+  square(180, 250, 30);
+  square(180, 310, 30);
+  square(180, 370, 30);
+  square(180, 430, 30);
+  square(180, 490, 30);
+  square(240, 130, 30);
+  square(240, 190, 30);
+  square(240, 250, 30);
+  square(240, 310, 30);
+  square(240, 370, 30);
+  square(240, 430, 30);
+  square(240, 490, 30);
+  fill("red");
+  rect(400, 400, 200, 100);
+}
